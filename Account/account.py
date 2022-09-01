a7ac87b02426cd367a6caeec0ac0518e3f57e656
Won <alyduho984530@gmail.com>
2022-09-01 18:39:15 +0900

    마진 계좌 잔고를 단위를 지정해서 가져오는 함수로 변경

--- Account/account.py
+++ Account/account.py
@@ -19,10 +19,20 @@ def login():
     API_KEY, SECRET_KEY = get_key()
     try:
         spot_client = Client(API_KEY, SECRET_KEY)
+        return spot_client
     except:
         print('로그인에 실패하였습니다. 올바른 API_KEY, SECRET_KEY인지 확인해주십시오.')
         
-def get_Margin_Account_KRW()  :
+def get_Margin_Account(unit = 'KRW')  :
     global spot_client
-    import pyupbit
-    return float(spot_client.margin_account()['totalAssetOfBtc']) * pyupbit.get_current_price(["KRW-BTC"])
+    if unit == 'KRW':
+        import pyupbit
+        return float(spot_client.margin_account()['totalAssetOfBtc']) * pyupbit.get_current_price(["KRW-BTC"])
+    elif unit == 'BTC' :
+        return float(spot_client.margin_account()['totalAssetOfBtc'])
+    elif unit == 'USDT' :
+        return float(spot_client.margin_account()['totalAssetOfBtc']) * float(spot_client.ticker_price("BTCUSDT")['price'])
+
+def test() :
+    spot_client = Client(base_url="https://testnet.binance.vision")
+    return spot_client
--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-from Account.account import login
-from Account.account import get_Margin_Account_KRW
+from Account.account import login, test, get_Margin_Account
 from binance.spot import Spot as Client
 
 login()
-print(get_Margin_Account_KRW())
