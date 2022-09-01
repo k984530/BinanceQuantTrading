b594af80d40863ac3c425dc3f94e2110ee676c6c
최원 <2017253020@yonsei.ac.kr>
2022-09-01 16:44:59 +0900

    마진 계좌 잔고 불러오기

--- Account/account.py
+++ Account/account.py
@@ -1,5 +1,9 @@
 from binance.spot import Spot as Client
 
+API_KEY = None
+SECRET_KEY = None
+spot_client = None
+
 def get_key():
     import os
     try:
@@ -11,8 +15,14 @@ def get_key():
     return API_KEY, SECRET_KEY
 
 def login():
+    global API_KEY, SECRET_KEY, spot_client
     API_KEY, SECRET_KEY = get_key()
     try:
-        spot_client = Client(API_KEY, SECRET_KEY, show_header=True)
+        spot_client = Client(API_KEY, SECRET_KEY)
     except:
         print('로그인에 실패하였습니다. 올바른 API_KEY, SECRET_KEY인지 확인해주십시오.')
+        
+def get_Margin_Account_KRW()  :
+    global spot_client
+    import pyupbit
+    return float(spot_client.margin_account()['totalAssetOfBtc']) * pyupbit.get_current_price(["KRW-BTC"])
