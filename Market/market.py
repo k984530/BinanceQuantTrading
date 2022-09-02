aee2cb0047b6b7e520a192c4b6a2902b825fe1d0
Won <alyduho984530@gmail.com>
2022-09-02 15:00:21 +0900

    __init__.py 추가, User class 수정

--- Market/market.py
+++ Market/market.py
@@ -2,4 +2,4 @@ from binance.spot import Spot as Client
 
 def get_price(symbol = 'BTCUSDT'):
     spot_client = Client(base_url="https://testnet.binance.vision")
-    return spot_client.ticker_price("BTCUSDT")
+    return spot_client.ticker_price(symbol)
