0b5f59a702e380e4279d91a4b8613123d889ebc0
최원 <2017253020@yonsei.ac.kr>
2022-09-03 16:43:34 +0900

    testing 함수 구현

--- testing.py
+++ testing.py
@@ -4,6 +4,9 @@ import pandas as pd
 from BackTesting import User
 from binance.spot import Spot as Client
 
-client = Account.login()
-Account.get_Margin_Account()
-print(Market.get_klines())
+a = [[1,2,3,4,5,6],[1,2,3,4,5,6]]
+b= pd.DataFrame(a, columns = ['a','b','c','d','e','f'])
+print(range(len(b)))
+for i in range(len(b)):
+    print(b.iloc[[i]])
+    print()
