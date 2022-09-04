0c5765040416c78501e8dbd6100e2a392f5a54fa
최원 <2017253020@yonsei.ac.kr>
2022-09-04 17:23:56 +0900

    staging

--- testing.py
+++ testing.py
@@ -4,15 +4,6 @@ import pandas as pd
 from BackTesting import User
 from binance.spot import Spot as Client
 
-<<<<<<< HEAD
 client = Account.login()
 Account.get_Margin_Account()
 print(Market.get_symbols())
-=======
-a = [[1,2,3,4,5,6],[1,2,3,4,5,6]]
-b= pd.DataFrame(a, columns = ['a','b','c','d','e','f'])
-print(range(len(b)))
-for i in range(len(b)):
-    print(b.iloc[[i]])
-    print()
->>>>>>> main
