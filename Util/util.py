ec6fcd819a90f6daae65bb76a4b11654621281fb
최원 <2017253020@yonsei.ac.kr>
2022-09-14 11:02:53 +0900

    정리

--- Util/util.py
+++ Util/util.py
@@ -1,6 +1,5 @@
 import os
 import requests as rq
-import pprint
 import pandas as pd
 API_KEY = os.environ['BINANCE_OPEN_API_KEY']
 SECRET_KEY = os.environ['BINANCE_OPEN_API_SECRET']
@@ -30,23 +29,24 @@ def get_account():
     account = rq.get(base_url + sub_url, headers = headers, params = param)
     return account.json()
 
-def get_market():
-    sub_url = '/sapi/v1/margin/allAssets'
-    data = rq.get(base_url + sub_url, headers = headers)
-    return data.json()
-
 def get_symbols() -> list:
     sub_url = '/fapi/v1/exchangeInfo'
     data = rq.get(base_url + sub_url, headers = headers)
     symbols = [symbol['symbol'] for symbol in data.json()['symbols'] if symbol['symbol'][-4:] == 'USDT']
     return symbols
 
-def get_kline(symbol = 'BTCUSDT', interval = '1m', limit = 10, ago = 0 ,endTime = get_server_time()) -> 'DataFrame':
+def get_kline(symbol = 'BTCUSDT', interval = '1m', limit = 10, unit = 0 ,endTime = get_server_time()) -> 'DataFrame':
     sub_url = '/fapi/v1/klines'
-    param = {'symbol' : symbol, 'interval' : interval, 'limit' : limit, 'endTime' : endTime}
+    startTime = endTime - (unit * 1000 * limit)
+    param = {'symbol' : symbol, 'interval' : interval, 'limit' : limit, 'startTime' : startTime,'endTime' : endTime}
     data = rq.get(base_url + sub_url, params = param).json()
     data = [i[:5] for i in data]
     df = pd.DataFrame(data, columns = ['시간','시가','고가','저가','종가'])
     return df
 
-print(get_market())
+def transfer_data(df):
+    df_time = df[['시간']].iloc[1:].reset_index(drop = True).astype('int64')
+    df_num = df[['시가','고가','저가','종가']].astype('float64')
+    df_num = (df_num.iloc[1:].reset_index(drop = True) - df_num.iloc[:-1].reset_index(drop = True)) / df_num.iloc[1:].reset_index(drop = True)
+    df = pd.concat([df_time,df_num],axis = 1)
+    return df
